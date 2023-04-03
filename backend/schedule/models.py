f174258cd60a9da5314a000510f80753dd6469fb
Charley <charley1135@gmail.com>
2023-04-03 15:00:12 -0700

    Feat: Created timeblock class.

--- backend/schedule/models.py
+++ backend/schedule/models.py
@@ -3,8 +3,6 @@ from authentication.models import User
 
 # Create your models here.
 
-class Timeblock(models.Model):
+class Schedule(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=240)
-    time = models.IntegerField()
-    is_active = models.BooleanField()
+    name = models.CharField(max_length=250)
